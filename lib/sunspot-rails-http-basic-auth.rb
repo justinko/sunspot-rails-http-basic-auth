12351be0b898654ff23c1c9c15d1e9a747217dcb
Justin Ko <jko170@gmail.com>
2011-05-27 11:52:21 -0600

    fix specs

--- lib/sunspot-rails-http-basic-auth.rb
+++ lib/sunspot-rails-http-basic-auth.rb
@@ -5,8 +5,6 @@ require 'sunspot-rails-http-basic-auth/rsolr/connection/net_http'
 require 'sunspot-rails-http-basic-auth/sunspot/rails'
 require 'sunspot-rails-http-basic-auth/sunspot/rails/configuration'
 
-
-if Rails::VERSION::MAJOR == 2
-  # reload the session that was already initialized when sunspot-rails gem loaded
+if defined?(Rails) && Rails::VERSION::MAJOR == 2
   Sunspot.session = Sunspot::Rails.build_session
 end
